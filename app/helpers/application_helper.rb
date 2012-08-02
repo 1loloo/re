edd109fea5ff4947515c232e5b733fba7d006e67
Sergey <cannaman@yandex.ru>
2012-08-02 20:44:36 +0400

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Коммерческая недвижимость"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,14 +7,19 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Коммерческая недвижимость'" do
+    it "should have the h1 'Коммерческая недвижимость'" do
       visit '/static_pages/home'
-      page.should have_content('Коммерческая недвижимость')
+      page.should have_selector('h1', :text => 'Коммерческая недвижимость')
     end
 
-    it "should have the title 'Главная'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Коммерческая недвижимость | Главная")
+      page.should have_selector('title', :text => "Коммерческая недвижимость")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Главная')
     end
 
   end
