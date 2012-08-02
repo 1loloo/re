bfac734ece59e0498ea29d7f555a586e89c9c4aa
Sergey <cannaman@yandex.ru>
2012-08-03 00:41:37 +0400

    Finish layout and routes 2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,1 @@
-#!/bin/env ruby
-# encoding: utf-8
-
-def full_title(page_title)
-  base_title = "Коммерческая недвижимость"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
